3f1dd156b65a10cdce5e9242b7b13d1d048612d5
Herdis Heida <herdisj24@ru.is>
2026-02-25 11:29:50 +0000

    interfaces and types for recipies

---
+++ my-app/src/types/recipe.ts
@@ -0,0 +1,26 @@
+export type RecipeTag = "Meat" | "Fish" | "Spicy" | "Chicken" | "Kid friendly";
+export type RecipeCategory = "Appetizer" | "Starter" | "Main Course" | "Side Dish" | "Dessert";
+
+export interface RecipeListItem {
+  id: string;
+  title: string;
+  image: string;
+  tags: RecipeTag[];
+  category: RecipeCategory; // TODO not sure if this is correct
+}
+
+export interface RecipeDetails extends RecipeListItem {
+  author: string;
+  description: string;
+  ingredients: string[];
+  instructions: string[];
+
+  // TODO not sure if this is correct
+  calories: number;
+  totalMinutes: number;
+}
+
+export interface RecipeType {
+  id: string;
+  title: string;
+}

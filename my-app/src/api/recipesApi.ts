3c8fcff79ce95587af129c92c1b7c50e994e9d10
Herdis Heida <herdisj24@ru.is>
2026-02-25 11:32:12 +0000

    fetching recipies

---
+++ my-app/src/api/recipesApi.ts
@@ -0,0 +1,22 @@
+/* getting recipes from base_url */
+
+const BASE_URL = "http://localhost:3500"; // change if needed
+
+async function safeFetch<T>(url: string): Promise<T> {
+    const res = await fetch(url);
+    if (!res.ok) throw new Error(`Request failed: ${res.status}`);
+    return res.json();
+}
+
+// TODO remove any type replac with correct types when they are defined in types/recipe.ts
+export function getRecipes() {
+    return safeFetch<any[]>(`${BASE_URL}/recipes`);
+}
+
+export function getRecipeTypes() {
+    return safeFetch<any[]>(`${BASE_URL}/recipes/recipeTypes`);
+}
+
+export function getRecipeById(recipeId: string) {
+    return safeFetch<any>(`${BASE_URL}/recipes/${recipeId}`);
+}

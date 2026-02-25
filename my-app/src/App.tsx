3cc88d1724a9611046770dfff56133b36f501c2e
Herdis Heida <herdisj24@ru.is>
2026-02-25 16:05:57 +0000

    app func with encased/encapsulated routes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,30 +1,11 @@
-import React from "react";
-
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes/AppRoutes";
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
-        {/* TODO navbar: search bar */}
-        {/* TODO main page */}
-        {/* TODO category select */}
-        {/* TODO list of recipeies */}
-        {/* TODO recipe info in detail */}
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 

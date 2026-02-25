3c10d927adba010207103d93334748391047ee40
Herdis Heida <herdisj24@ru.is>
2026-02-25 15:55:56 +0000

    error/loading msg draft

---
+++ my-app/src/components/ui/ErrorMessage.tsx
@@ -0,0 +1,7 @@
+export function ErrorMessage({ message }: { message: string }) {
+  return (
+    <div style={{ padding: 16, color: "crimson" }}>
+      {message}
+    </div>
+  );
+}
---
+++ my-app/src/components/ui/Loading.tsx
@@ -0,0 +1,3 @@
+export function Loading() {
+  return <div style={{ padding: 16 }}>Loading…</div>;
+}

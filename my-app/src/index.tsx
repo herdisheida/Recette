e4db0823cf3608ab9b165ef165fe98a9d38290b4
Herdis Heida <herdisj24@ru.is>
2026-02-28 22:47:40 +0000

    delete unnecessary
    það sem kom þegar við bjuggum til templateið

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 import "./styles/global.css";
 import "./styles/variables.css";
-
-import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,

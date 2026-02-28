e3fba91fe3172a9eff5ce16dc52aeada36881648
Herdis Heida <herdisj24@ru.is>
2026-02-28 18:23:38 +0000

    add variables to already css thingies

--- my-app/src/components/layout/Header.tsx
+++ my-app/src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import styles from "./Header.module.css";
 export function Header({ children }: { children: React.ReactNode }) {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>Explore Recipes</h1>
+      <p className={styles.title}>Explore Recipes</p>
       <div className={styles.controls}>{children}</div>
     </header>
   );
--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import "./styles/global.css";
+import "./styles/variables.css";
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(
